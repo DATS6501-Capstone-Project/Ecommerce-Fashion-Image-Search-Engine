aa3ec5d6cf1dbf2923373fb538a61bb166e23f83
gayuc07 <54827613+gayuc07@users.noreply.github.com>
2021-03-07 12:26:35 -0500

    Add files via upload

--- Webapp/webapp_sample.py
+++ Webapp/webapp_sample.py
@@ -1,7 +1,8 @@
 import streamlit as st
 from PIL import Image
 import os
-
+import pandas as pd
+from streamlit_cropper import st_cropper
 ### Excluding Imports ###
 st.title("E-Commerce Fashion Visual Search - Category type detection")
 
@@ -12,17 +13,38 @@ if uploaded_file!=None:
     # img = cv2.imread(img)
     image = Image.open(uploaded_file)
     (w,h) = image.size # just the way PIL works - cv2 does h,w
-    col1, col2, col3 = st.beta_columns(3)
-    with col2:
-        st.header("Uploaded Image.")
-        st.image(image)
+    st.header("Uploaded Image.")
+    cropped_img = st_cropper(image)
     os.chdir("/home/ubuntu/capstone/check_new/darknet")
-    with open("./data/test_images.jpg","wb") as f:
-        f.write(uploaded_file.getbuffer())
-    test_image = 'data/test_images.jpg'
-    command_darknet = "./darknet detector test data/obj.data cfg/yolo-obj.cfg yolo-obj_final.weights -dont_show -thresh 0.3 " + test_image
+    cropped_img.save("./data/test_images.jpg")
+    command_darknet = "./darknet detector test data/obj.data cfg/yolo-obj.cfg yolo-obj_final.weights -dont_show -thresh 0.3 -out result.json < test_sample.txt"
     os.system(command_darknet)
-    predict_image = Image.open('predictions.jpg')
-    with col2:
-        st.header("Predicted Image.")
-        st.image(predict_image)
+    results = pd.read_json("./result.json")
+    for i in results['objects']:
+        if len(i) > 1:
+            views = 1
+            for clas in i:
+                if clas['confidence'] <= 0.40:
+                    views = 0
+            if views != 0:
+                predict_image = Image.open('predictions.jpg')
+                st.header("Predicted Image.")
+                st.image(predict_image)
+            else:
+                st.warning("Detected Object category exceeds maximum limit or No appropriate product category detected - please try again with new image or modify the crop box to fit exact area of interest")
+        elif len(i) == 0:
+            st.warning("Detected Object category exceeds maximum limit or No appropriate product category detected - please try again with new image or modify the crop box to fit exact area of interest")
+        elif len(i) == 1:
+            views = 1
+            for clas in i:
+                if clas['confidence'] <= 0.70:
+                    st.warning("Detected Object category exceeds maximum limit or No appropriate product category detected - please try again with new image or modify the crop box to fit exact area of interest")
+                else:
+                    predict_image = Image.open('predictions.jpg')
+                    st.header("Predicted Image.")
+                    st.image(predict_image)
+
+
+
+
+
